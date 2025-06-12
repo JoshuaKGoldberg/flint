2943004f694d980ec3ed2211c5e60d5af1bc5736
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2025-06-12 13:38:43 -0400

    feat: add --help and --version to CLI (#71)
    ## PR Checklist
    
    - [x] Addresses an existing open issue: fixes #58
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/JoshuaKGoldberg/flint/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/JoshuaKGoldberg/flint/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    ❤️‍🔥

--- src/cli/runCli.ts
+++ src/cli/runCli.ts
@@ -1,13 +1,41 @@
 import { debugForFile } from "debug-for-file";
+import { parseArgs } from "node:util";
 
 import { isConfig } from "../configs/isConfig.js";
 import { plainReporter } from "../reporters/plain.js";
 import { runConfig } from "../running/lintConfig.js";
 import { findConfigFileName } from "./findConfigFileName.js";
+import { packageData } from "./packageData.js";
 
 const log = debugForFile(import.meta.filename);
 
 export async function runCli() {
+	const { values } = parseArgs({
+		options: {
+			help: {
+				type: "boolean",
+			},
+			version: {
+				type: "boolean",
+			},
+		},
+		strict: true,
+	});
+
+	if (values.help) {
+		console.log("Welcome to Flint!");
+		console.log("Flint is still very early stage and experimental.");
+		console.log(
+			"See \u001b]8;;http://github.com/JoshuaKGoldberg/flint\u0007github.com/JoshuaKGoldberg/flint\u001b]8;;\u0007 for more information.",
+		);
+		return 0;
+	}
+
+	if (values.version) {
+		console.log(packageData.version);
+		return 0;
+	}
+
 	const configFileName = await findConfigFileName(process.cwd());
 	if (!configFileName) {
 		console.error("No flint.config.* file found");
