a4b07b17b9cadc1d5c51eed3c359a73f28333da8
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2025-07-18 18:37:23 -0500

    fix(core): remove TypeScript properties from RuleContext (#179)
    <!-- 👋 Hi, thanks for sending a PR to flint! 
    Please fill out all fields below and make sure each item is true and [x]
    checked.
    Otherwise we may not be able to review your PR. -->
    
    ## PR Checklist
    
    - [x] Addresses an existing open issue: fixes #178
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/JoshuaKGoldberg/flint/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/JoshuaKGoldberg/flint/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    Removes the offending properties.
    
    ❤️‍🔥

---
+++ .changeset/eleven-wolves-fix.md
@@ -0,0 +1,5 @@
+---
+"@flint.fyi/core": patch
+---
+
+remove TypeScript properties from RuleContext
--- packages/core/src/types/context.ts
+++ packages/core/src/types/context.ts
@@ -1,11 +1,7 @@
-import type * as ts from "typescript";
-
 import { RuleReport } from "./reports.js";
 
 export interface RuleContext<MessageId extends string> {
 	report: RuleReporter<MessageId>;
-	sourceFile: ts.SourceFile;
-	typeChecker: ts.TypeChecker;
 }
 
 export type RuleReporter<MessageId extends string> = (
