7a8f52f90227646649d195439888b2ed2e28e466
Josh Goldberg <git@joshuakgoldberg.com>
2025-08-14 21:18:07 -0600

    feat(flint): use Node's enableCompileCache

--- packages/flint/bin/index.js
+++ packages/flint/bin/index.js
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
 import { runCli } from "@flint.fyi/cli";
+import { enableCompileCache } from "node:module";
+
+enableCompileCache();
 
 process.exitCode = await runCli(process.argv.slice(2));
