88c094b418c49d989fa3e64f49533e7970bbfb7d
Josh Goldberg <git@joshuakgoldberg.com>
2025-10-10 12:42:45 -0400

    tweaks

--- packages/ts/src/rules/sparseArrays.ts
+++ packages/ts/src/rules/sparseArrays.ts
@@ -24,56 +24,61 @@ export default typescriptLanguage.createRule({
 		},
 	},
 	setup(context) {
+		function findCommaPositions(
+			node: ts.ArrayLiteralExpression,
+			startingIndex: number,
+		) {
+			const searchStart =
+				startingIndex === 0
+					? node.getStart(context.sourceFile) + 1
+					: node.elements[startingIndex - 1].getEnd();
+
+			let first = -1;
+			let hole: number | undefined;
+
+			for (let j = searchStart; j < context.sourceFile.text.length; j++) {
+				if (context.sourceFile.text[j] === ",") {
+					if (first === -1) {
+						first = j;
+					} else {
+						hole = j;
+						break;
+					}
+				}
+			}
+
+			return { first, hole };
+		}
+
 		return {
 			visitors: {
 				ArrayLiteralExpression: (node) => {
 					for (let i = 0; i < node.elements.length; i++) {
 						const element = node.elements[i];
-						if (ts.isOmittedExpression(element)) {
-							// Find the comma that represents this hole
-							// For a hole like [1, , 3], there are two commas:
-							// - The first comma after the previous element
-							// - The second comma representing the hole itself
-							// We want to report on the second comma
+						if (!ts.isOmittedExpression(element)) {
+							continue;
+						}
 
-							const searchStart =
-								i === 0
-									? node.getStart(context.sourceFile) + 1
-									: node.elements[i - 1].getEnd();
+						const commaPositions = findCommaPositions(node, i);
 
-							let firstCommaPos = -1;
-							let holeCommaPos = -1;
+						// Find the comma that represents this hole
+						// For a hole like [1, , 3], there are two commas:
+						// - The first comma after the previous element
+						// - The second comma representing the hole itself
+						// We want to report on the second comma
+						// For holes at the beginning like [, 2], there's only one comma
+						const commaToReport = commaPositions.hole ?? commaPositions.first;
 
-							for (
-								let j = searchStart;
-								j < context.sourceFile.text.length;
-								j++
-							) {
-								if (context.sourceFile.text[j] === ",") {
-									if (firstCommaPos === -1) {
-										firstCommaPos = j;
-									} else {
-										holeCommaPos = j;
-										break;
-									}
-								}
-							}
+						if (commaToReport !== -1) {
+							const range = {
+								begin: commaToReport,
+								end: commaToReport + 1,
+							};
 
-							// For holes at the beginning like [, 2], there's only one comma
-							const commaToReport =
-								holeCommaPos !== -1 ? holeCommaPos : firstCommaPos;
-
-							if (commaToReport !== -1) {
-								const range = {
-									begin: commaToReport,
-									end: commaToReport + 1,
-								};
-
-								context.report({
-									message: "noSparseArray",
-									range,
-								});
-							}
+							context.report({
+								message: "noSparseArray",
+								range,
+							});
 						}
 					}
 				},
