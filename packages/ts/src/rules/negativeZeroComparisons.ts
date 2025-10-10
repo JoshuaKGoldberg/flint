310edd839d79840ab22b3d4ef74bf1dc5a81b35e
Josh Goldberg <git@joshuakgoldberg.com>
2025-10-10 12:53:11 -0400

    Tweaks

--- packages/ts/src/rules/negativeZeroComparisons.ts
+++ packages/ts/src/rules/negativeZeroComparisons.ts
@@ -2,6 +2,26 @@ import * as ts from "typescript";
 
 import { typescriptLanguage } from "../language.js";
 
+const comparisonOperators = new Set([
+	ts.SyntaxKind.EqualsEqualsEqualsToken,
+	ts.SyntaxKind.EqualsEqualsToken,
+	ts.SyntaxKind.ExclamationEqualsEqualsToken,
+	ts.SyntaxKind.ExclamationEqualsToken,
+	ts.SyntaxKind.GreaterThanEqualsToken,
+	ts.SyntaxKind.GreaterThanToken,
+	ts.SyntaxKind.LessThanEqualsToken,
+	ts.SyntaxKind.LessThanToken,
+]);
+
+function isNegativeZero(node: ts.Node): boolean {
+	return (
+		ts.isPrefixUnaryExpression(node) &&
+		node.operator === ts.SyntaxKind.MinusToken &&
+		ts.isNumericLiteral(node.operand) &&
+		node.operand.text === "0"
+	);
+}
+
 export default typescriptLanguage.createRule({
 	about: {
 		description: "Reports comparisons with -0 that may not behave as expected.",
@@ -14,39 +34,19 @@ export default typescriptLanguage.createRule({
 				"Comparisons with -0 using {{ operator }} do not distinguish between -0 and +0.",
 			secondary: [
 				"In JavaScript, -0 and +0 are considered equal when using comparison operators like === or ==, even though they are distinct values.",
-				"To properly check for -0, use Object.is(value, -0) which correctly distinguishes between -0 and +0.",
+				"To properly check for -0, use `Object.is(value, -0)` which correctly distinguishes between -0 and +0.",
 			],
-			suggestions: ["Use Object.is() to reliably check if a value is -0."],
+			suggestions: ["Use `Object.is()` to reliably check if a value is -0."],
 		},
 	},
 	setup(context) {
-		function isNegativeZero(node: ts.Node): boolean {
-			return (
-				ts.isPrefixUnaryExpression(node) &&
-				node.operator === ts.SyntaxKind.MinusToken &&
-				ts.isNumericLiteral(node.operand) &&
-				node.operand.text === "0"
-			);
-		}
-
 		return {
 			visitors: {
 				BinaryExpression: (node) => {
 					if (
-						node.operatorToken.kind !== ts.SyntaxKind.EqualsEqualsToken &&
-						node.operatorToken.kind !== ts.SyntaxKind.EqualsEqualsEqualsToken &&
-						node.operatorToken.kind !== ts.SyntaxKind.ExclamationEqualsToken &&
-						node.operatorToken.kind !==
-							ts.SyntaxKind.ExclamationEqualsEqualsToken &&
-						node.operatorToken.kind !== ts.SyntaxKind.LessThanToken &&
-						node.operatorToken.kind !== ts.SyntaxKind.LessThanEqualsToken &&
-						node.operatorToken.kind !== ts.SyntaxKind.GreaterThanToken &&
-						node.operatorToken.kind !== ts.SyntaxKind.GreaterThanEqualsToken
+						!comparisonOperators.has(node.operatorToken.kind) ||
+						(!isNegativeZero(node.left) && !isNegativeZero(node.right))
 					) {
-						return;
-					}
-
-					if (!isNegativeZero(node.left) && !isNegativeZero(node.right)) {
 						return;
 					}
 
