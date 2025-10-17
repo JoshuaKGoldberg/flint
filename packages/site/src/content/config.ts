41dd46494b57c50aacdbbe4383870657ca6e8589
Josh Goldberg <git@joshuakgoldberg.com>
2025-10-17 07:31:17 -0400

    revert: remove unnecessary astro config eslint-disable comment

--- packages/site/src/content/config.ts
+++ packages/site/src/content/config.ts
@@ -4,7 +4,6 @@ import { defineCollection } from "astro:content";
 import { topicSchema } from "starlight-sidebar-topics/schema";
 
 export const collections = {
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
 	docs: defineCollection({
 		loader: docsLoader(),
 		schema: docsSchema({ extend: topicSchema }),
