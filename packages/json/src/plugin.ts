34b35e55820a79f59db69182c5596734782d9a5e
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2025-10-17 08:57:54 -0400

    feat: implement valueSafety for JSON plugin (#558)
    ## PR Checklist
    
    - [x] Addresses an existing open issue: fixes #556
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/JoshuaKGoldberg/flint/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/JoshuaKGoldberg/flint/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    ❤️‍🔥

--- packages/json/src/plugin.ts
+++ packages/json/src/plugin.ts
@@ -2,11 +2,12 @@ import { createPlugin } from "@flint.fyi/core";
 
 import keyDuplicates from "./rules/keyDuplicates.js";
 import keyNormalization from "./rules/keyNormalization.js";
+import valueSafety from "./rules/valueSafety.js";
 
 export const json = createPlugin({
 	files: {
 		all: ["**/*.json"],
 	},
 	name: "json",
-	rules: [keyDuplicates, keyNormalization],
+	rules: [keyDuplicates, keyNormalization, valueSafety],
 });
