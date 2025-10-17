d2d84908d19ca12e65c0283ad4834949103374dc
Josh Goldberg <git@joshuakgoldberg.com>
2025-10-17 13:27:53 -0400

    feat: implement labelReferences for Markdown plugin

---
+++ packages/md/src/rules/labelReferences.test.ts
@@ -0,0 +1,100 @@
+import rule from "./labelReferences.js";
+import { ruleTester } from "./ruleTester.js";
+
+ruleTester.describe(rule, {
+	invalid: [
+		{
+			code: `
+[ESLint][eslint]
+`,
+			snapshot: `
+[ESLint][eslint]
+~~~~~~~~~~~~~~~~
+This label reference 'eslint' has no definition.
+`,
+		},
+		{
+			code: `
+[eslint][]
+`,
+			snapshot: `
+[eslint][]
+~~~~~~~~~~
+This label reference 'eslint' has no definition.
+`,
+		},
+		{
+			code: `
+[eslint]
+`,
+			snapshot: `
+[eslint]
+~~~~~~~~
+This label reference 'eslint' has no definition.
+`,
+		},
+		{
+			code: `
+![Logo][logo]
+`,
+			snapshot: `
+![Logo][logo]
+~~~~~~~~~~~~~
+This label reference 'logo' has no definition.
+`,
+		},
+		{
+			code: `
+[Valid link][valid]
+
+[valid]: https://example.com
+
+[Missing link][missing]
+`,
+			snapshot: `
+[Valid link][valid]
+
+[valid]: https://example.com
+
+[Missing link][missing]
+~~~~~~~~~~~~~~~~~~~~~~~
+This label reference 'missing' has no definition.
+`,
+		},
+	],
+	valid: [
+		`
+[ESLint][eslint]
+
+[eslint]: https://eslint.org
+`,
+		`
+[eslint][]
+
+[eslint]: https://eslint.org
+`,
+		`
+[eslint]
+
+[eslint]: https://eslint.org
+`,
+		`
+![Logo][logo]
+
+[logo]: ./logo.png
+`,
+		`
+[First][first]
+[Second][second]
+
+[first]: https://example.com/1
+[second]: https://example.com/2
+`,
+		`
+Check out [this link][ref] and ![this image][img].
+
+[ref]: https://example.com
+[img]: image.png
+`,
+	],
+});
