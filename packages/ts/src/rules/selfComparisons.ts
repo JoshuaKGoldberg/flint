4e3e76fb947d6d7701c05d6070513b20f4b423ab
Josh Goldberg <git@joshuakgoldberg.com>
2025-10-17 07:41:45 -0400

    perf: optimize hasSameTokens to iterate in tandem and bail early on mismatch

--- packages/ts/src/rules/selfComparisons.ts
+++ packages/ts/src/rules/selfComparisons.ts
@@ -3,37 +3,54 @@ import * as ts from "typescript";
 import { typescriptLanguage } from "../language.js";
 import { isComparisonOperator } from "./utils/operators.js";
 
+function* getTokens(node: ts.Node): Generator<ts.Node> {
+	if (
+		node.kind >= ts.SyntaxKind.FirstToken &&
+		node.kind <= ts.SyntaxKind.LastToken
+	) {
+		yield node;
+		return;
+	}
+
+	// Recursively yield tokens from children
+	const children: ts.Node[] = [];
+	ts.forEachChild(node, (child) => {
+		children.push(child);
+	});
+
+	for (const child of children) {
+		yield* getTokens(child);
+	}
+}
+
 function hasSameTokens(
 	nodeA: ts.Node,
 	nodeB: ts.Node,
 	sourceFile: ts.SourceFile,
 ): boolean {
-	const tokensA: ts.Node[] = [];
-	const tokensB: ts.Node[] = [];
+	const iteratorA = getTokens(nodeA);
+	const iteratorB = getTokens(nodeB);
 
-	function collectTokens(node: ts.Node, tokens: ts.Node[]) {
+	let resultA = iteratorA.next();
+	let resultB = iteratorB.next();
+
+	while (!resultA.done && !resultB.done) {
+		const tokenA = resultA.value;
+		const tokenB = resultB.value;
+
 		if (
-			node.kind >= ts.SyntaxKind.FirstToken &&
-			node.kind <= ts.SyntaxKind.LastToken
+			tokenA.kind !== tokenB.kind ||
+			tokenA.getText(sourceFile) !== tokenB.getText(sourceFile)
 		) {
-			tokens.push(node);
+			return false;
 		}
-		ts.forEachChild(node, (child) => {
-			collectTokens(child, tokens);
-		});
+
+		resultA = iteratorA.next();
+		resultB = iteratorB.next();
 	}
 
-	collectTokens(nodeA, tokensA);
-	collectTokens(nodeB, tokensB);
-
-	return (
-		tokensA.length === tokensB.length &&
-		tokensA.every(
-			(token, index) =>
-				token.kind === tokensB[index].kind &&
-				token.getText(sourceFile) === tokensB[index].getText(sourceFile),
-		)
-	);
+	// Both iterators should be exhausted at the same time
+	return resultA.done === resultB.done;
 }
 
 export default typescriptLanguage.createRule({
