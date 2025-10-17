9c2d2081bc33c4dcf841b7bf3b95a8fbf076bfea
Josh Goldberg <git@joshuakgoldberg.com>
2025-10-17 13:44:06 -0400

    feat: implement linkFragments for Markdown plugin

--- packages/md/src/plugin.ts
+++ packages/md/src/plugin.ts
@@ -2,11 +2,12 @@ import { createPlugin } from "@flint.fyi/core";
 
 import bareUrls from "./rules/bareUrls.js";
 import headingIncrements from "./rules/headingIncrements.js";
+import linkFragments from "./rules/linkFragments.js";
 
 export const md = createPlugin({
 	files: {
 		all: ["**/*.md"],
 	},
 	name: "md",
-	rules: [bareUrls, headingIncrements],
+	rules: [bareUrls, headingIncrements, linkFragments],
 });
