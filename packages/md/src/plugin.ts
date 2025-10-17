99d131d7d08f8548e919829855d3c5bdab56c31a
Josh Goldberg <git@joshuakgoldberg.com>
2025-10-17 13:30:34 -0400

    feat: implement labelReferenceValidity for Markdown plugin

--- packages/md/src/plugin.ts
+++ packages/md/src/plugin.ts
@@ -2,11 +2,12 @@ import { createPlugin } from "@flint.fyi/core";
 
 import bareUrls from "./rules/bareUrls.js";
 import headingIncrements from "./rules/headingIncrements.js";
+import labelReferenceValidity from "./rules/labelReferenceValidity.js";
 
 export const md = createPlugin({
 	files: {
 		all: ["**/*.md"],
 	},
 	name: "md",
-	rules: [bareUrls, headingIncrements],
+	rules: [bareUrls, headingIncrements, labelReferenceValidity],
 });
