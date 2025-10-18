e13c45497338e24e35a60f59b75afc9e1e4fc348
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2025-10-17 23:34:15 -0400

    feat: implement iframeTitles for JSX plugin (#594)
    ## PR Checklist
    
    - [x] Addresses an existing open issue: fixes #512
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/JoshuaKGoldberg/flint/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/JoshuaKGoldberg/flint/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    ❤️‍🔥

--- packages/plugin-jsx/src/jsx.ts
+++ packages/plugin-jsx/src/jsx.ts
@@ -2,8 +2,9 @@ import { createPlugin } from "@flint.fyi/core";
 
 import accessKeys from "./rules/accessKeys.js";
 import htmlLangs from "./rules/htmlLangs.js";
+import iframeTitles from "./rules/iframeTitles.js";
 
 export const jsx = createPlugin({
 	name: "jsx",
-	rules: [accessKeys, htmlLangs],
+	rules: [accessKeys, htmlLangs, iframeTitles],
 });
