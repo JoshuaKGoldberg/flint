caf1ca319c4dee73ddcd5838f5322244fb02c919
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2025-10-17 23:59:28 -0400

    feat: implement distractingElements for JSX plugin (#596)
    ## PR Checklist
    
    - [x] Addresses an existing open issue: fixes #514
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/JoshuaKGoldberg/flint/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/JoshuaKGoldberg/flint/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    ❤️‍🔥

--- packages/plugin-jsx/src/jsx.ts
+++ packages/plugin-jsx/src/jsx.ts
@@ -1,10 +1,11 @@
 import { createPlugin } from "@flint.fyi/core";
 
 import accessKeys from "./rules/accessKeys.js";
+import distractingElements from "./rules/distractingElements.js";
 import htmlLangs from "./rules/htmlLangs.js";
 import iframeTitles from "./rules/iframeTitles.js";
 
 export const jsx = createPlugin({
 	name: "jsx",
-	rules: [accessKeys, htmlLangs, iframeTitles],
+	rules: [accessKeys, distractingElements, htmlLangs, iframeTitles],
 });
