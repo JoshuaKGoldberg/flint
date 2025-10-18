03a0724834fd38a698f712ea78b7b12477e9fa5a
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2025-10-17 23:39:55 -0400

    feat: implement imageContents for Markdown plugin (#580)
    ## PR Checklist
    
    - [x] Addresses an existing open issue: fixes #563
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/JoshuaKGoldberg/flint/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/JoshuaKGoldberg/flint/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    ❤️‍🔥

--- packages/md/src/plugin.ts
+++ packages/md/src/plugin.ts
@@ -3,11 +3,12 @@ import { createPlugin } from "@flint.fyi/core";
 import bareUrls from "./rules/bareUrls.js";
 import fencedCodeLanguages from "./rules/fencedCodeLanguages.js";
 import headingIncrements from "./rules/headingIncrements.js";
+import imageContents from "./rules/imageContents.js";
 
 export const md = createPlugin({
 	files: {
 		all: ["**/*.md"],
 	},
 	name: "md",
-	rules: [bareUrls, fencedCodeLanguages, headingIncrements],
+	rules: [bareUrls, fencedCodeLanguages, headingIncrements, imageContents],
 });
