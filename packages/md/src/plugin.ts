3ece58ffd2b416d8aeab0b1a6fda175c1260e970
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2025-10-20 07:51:33 -0400

    feat: implement definitionDuplicates for Markdown plugin (#577)
    ## PR Checklist
    
    - [x] Addresses an existing open issue: fixes #560
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/JoshuaKGoldberg/flint/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/JoshuaKGoldberg/flint/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    Implements the `definitionDuplicates` rule for the Markdown plugin,
    which reports duplicate definition identifiers (case-insensitive).
    
    This rule allows comment-style definitions (e.g., `[//]:`) by default,
    matching ESLint's markdown plugin behavior.

--- packages/md/src/plugin.ts
+++ packages/md/src/plugin.ts
@@ -2,6 +2,7 @@ import { createPlugin } from "@flint.fyi/core";
 
 import bareUrls from "./rules/bareUrls.js";
 import definitionContents from "./rules/definitionContents.js";
+import definitionDuplicates from "./rules/definitionDuplicates.js";
 import fencedCodeLanguages from "./rules/fencedCodeLanguages.js";
 import headingIncrements from "./rules/headingIncrements.js";
 import imageAltTexts from "./rules/imageAltTexts.js";
@@ -15,6 +16,7 @@ export const md = createPlugin({
 	name: "md",
 	rules: [
 		bareUrls,
+		definitionDuplicates,
 		definitionContents,
 		fencedCodeLanguages,
 		headingIncrements,
