d568f13ed186ffe445ba98c19334cfa22ad35155
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2025-10-21 09:35:30 -0500

    feat: implement mediaCaptions for JSX plugin (#603)
    ## PR Checklist
    
    - [x] Addresses an existing open issue: fixes #531
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/JoshuaKGoldberg/flint/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/JoshuaKGoldberg/flint/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    ❤️‍🔥

--- packages/plugin-jsx/src/jsx.ts
+++ packages/plugin-jsx/src/jsx.ts
@@ -5,11 +5,16 @@ import altTexts from "./rules/altTexts.js";
 import distractingElements from "./rules/distractingElements.js";
 import htmlLangs from "./rules/htmlLangs.js";
 import iframeTitles from "./rules/iframeTitles.js";
+import mediaCaptions from "./rules/mediaCaptions.js";
 import mouseEventKeyEvents from "./rules/mouseEventKeyEvents.js";
 
 export const jsx = createPlugin({
 	name: "jsx",
 	rules: [
+		accessKeys,
+		htmlLangs,
+		iframeTitles,
+		mediaCaptions,
 		accessKeys,
 		altTexts,
 		distractingElements,
