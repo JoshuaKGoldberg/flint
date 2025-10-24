d89c4808b94fee91891bdceb476c1ce1c8318db1
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2025-10-24 14:39:09 -0500

    feat(comparisons): add eslint-plugin-eslint-plugin comparisons (#709)
    ## PR Checklist
    
    - [x] Addresses an existing open issue: fixes #343
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/JoshuaKGoldberg/flint/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/JoshuaKGoldberg/flint/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    Additionally renames `duplicateTestCases` to `testCaseDuplicates`, to
    align with the Flint precedent of putting the area/group part of the
    rule name first.
    
    ❤️‍🔥

---
+++ .changeset/dark-pants-repair.md
@@ -0,0 +1,6 @@
+---
+"@flint.fyi/plugin-flint": minor
+"@flint.fyi/comparisons": minor
+---
+
+feat(comparisons): add eslint-plugin-eslint-plugin comparisons
--- packages/plugin-flint/src/rules/testCaseDuplicates.test.ts
+++ packages/plugin-flint/src/rules/testCaseDuplicates.test.ts
@@ -1,5 +1,5 @@
-import rule from "./duplicateTestCases.js";
 import { ruleTester } from "./ruleTester.js";
+import rule from "./testCaseDuplicates.js";
 
 ruleTester.describe(rule, {
 	invalid: [
