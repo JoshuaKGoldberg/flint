25d3a443093844835604cf850dfdbbe0337a1585
auvred <61150013+auvred@users.noreply.github.com>
2025-11-24 21:22:57 +0300

    fix: resolve correct type for `createPlugin(...).files` (#1005)
    <!-- 👋 Hi, thanks for sending a PR to flint! ❤️‍🔥
    Please fill out all fields below and make sure each item is true and [x]
    checked.
    Otherwise we may not be able to review your PR. -->
    
    ## PR Checklist
    
    - [x] Addresses an existing open issue: fixes #1004
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/JoshuaKGoldberg/flint/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/JoshuaKGoldberg/flint/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    <!-- Description of what is changed and how the code change does that.
    -->
    
    ---------
    
    Co-authored-by: Josh Goldberg <git@joshuakgoldberg.com>

--- flint.config.ts
+++ flint.config.ts
@@ -2,30 +2,27 @@ import { cspell } from "@flint.fyi/plugin-cspell";
 import { flint } from "@flint.fyi/plugin-flint";
 import { defineConfig, globs, json, md, ts, yaml } from "flint";
 
-// TODO: How to get the globs types piped through?
-// eslint-disable-next-line @eslint-community/eslint-comments/disable-enable-pair
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
 export default defineConfig({
 	use: [
 		{
-			files: json.files!.all,
+			files: json.files.all,
 			rules: json.presets.logical,
 		},
 		{
-			files: md.files!.all,
+			files: md.files.all,
 			rules: md.presets.logical,
 		},
 		{
 			files: {
 				exclude: process.env.LINT_FIXTURES ? [] : ["packages/fixtures"],
-				include: ts.files!.all,
+				include: ts.files.all,
 			},
 			rules: [flint.presets.logical, ts.presets.logical],
 		},
 		{
 			files: {
 				exclude: ["pnpm-lock.yaml"],
-				include: yaml.files!.all,
+				include: yaml.files.all,
 			},
 			rules: yaml.presets.logical,
 		},
