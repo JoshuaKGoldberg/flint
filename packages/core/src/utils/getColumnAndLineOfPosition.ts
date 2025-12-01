1bbae2e3e5a1e8886fe13235160184bb5be5f8af
auvred <61150013+auvred@users.noreply.github.com>
2025-12-01 17:24:48 +0300

    feat(core): add `binarySearch` and `getLineAndColumnOfPosition` utilities (#1032)
    <!-- 👋 Hi, thanks for sending a PR to flint! ❤️‍🔥
    Please fill out all fields below and make sure each item is true and [x]
    checked.
    Otherwise we may not be able to review your PR. -->
    
    ## PR Checklist
    
    - [x] Addresses an existing open issue: fixes #1010
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/JoshuaKGoldberg/flint/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/JoshuaKGoldberg/flint/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    <!-- Description of what is changed and how the code change does that.
    -->

---
+++ packages/core/src/utils/getColumnAndLineOfPosition.ts
@@ -0,0 +1,124 @@
+import { ColumnAndLine } from "../types/ranges.js";
+import { binarySearch } from "./arrays.js";
+
+/** Subset of ts.SourceFileLike */
+export interface HasGetLineAndCharacterOfPosition {
+	getLineAndCharacterOfPosition(position: number): TSLineAndCharacter;
+}
+
+export interface SourceFileWithLineMap {
+	readonly text: string;
+	/** Used for caching the start positions of lines in `text` */
+	lineMap?: readonly number[];
+}
+
+/**
+ * Structurally the same as ts.LineAndCharacter.
+ * It's duplicated because `@flint.fyi/core` doesn't have a dependency on `typescript`.
+ */
+export interface TSLineAndCharacter {
+	/** 0-based. */
+	character: number;
+	line: number;
+}
+
+/**
+ * Prefer passing a `source` of type `HasGetLineAndCharacterOfPosition` or `SourceFileWithLineMap`.
+ * This way, the expensive computation of the `lineMap` will be cached across multiple calls.
+ */
+export function getColumnAndLineOfPosition(
+	source: HasGetLineAndCharacterOfPosition | SourceFileWithLineMap | string,
+	position: number,
+): ColumnAndLine {
+	if (typeof source === "string") {
+		return computeColumnAndLineOfPosition(
+			source,
+			computeLineStarts(source),
+			position,
+		);
+	}
+	if ("getLineAndCharacterOfPosition" in source) {
+		const { character, line } = source.getLineAndCharacterOfPosition(position);
+		return { column: character, line, raw: position };
+	}
+	source.lineMap ??= computeLineStarts(source.text);
+	return computeColumnAndLineOfPosition(source.text, source.lineMap, position);
+}
+
+function computeColumnAndLineOfPosition(
+	source: string,
+	lineStarts: readonly number[],
+	position: number,
+): ColumnAndLine {
+	if (position < 0) {
+		return {
+			column: 0,
+			line: 0,
+			raw: 0,
+		};
+	}
+	if (position > source.length) {
+		const line = lineStarts.length - 1;
+		return {
+			column: Math.max(source.length - lineStarts[line], 0),
+			line,
+			raw: Math.max(source.length - 1, 0),
+		};
+	}
+	const { element: lineStart, index: line } = binarySearch(
+		lineStarts,
+		(start) => (start < position ? -1 : start > position ? 1 : 0),
+		"fallback-prev",
+	);
+
+	return {
+		// At this point, position is always >0, and lineStarts is always
+		// non-empty, so with "fallback-prev", binarySearch always returns
+		// a non-undefined element.
+		// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+		column: position - lineStart!,
+		line,
+		raw: position,
+	};
+}
+function computeLineStarts(source: string): readonly number[] {
+	const res = [];
+	let lineStart = 0;
+	let cr = source.indexOf("\r", lineStart);
+	let lf = source.indexOf("\n", lineStart);
+	while (true) {
+		if (lf === -1) {
+			while (cr !== -1) {
+				res.push(lineStart);
+				lineStart = cr + 1;
+				cr = source.indexOf("\r", lineStart);
+			}
+			break;
+		}
+		if (cr === -1) {
+			while (lf !== -1) {
+				res.push(lineStart);
+				lineStart = lf + 1;
+				lf = source.indexOf("\n", lineStart);
+			}
+			break;
+		}
+		if (cr + 1 === lf) {
+			res.push(lineStart);
+			lineStart = lf + 1;
+			cr = source.indexOf("\r", lineStart);
+			lf = source.indexOf("\n", lineStart);
+			continue;
+		}
+		res.push(lineStart);
+		if (cr < lf) {
+			lineStart = cr + 1;
+			cr = source.indexOf("\r", lineStart);
+		} else {
+			lineStart = lf + 1;
+			lf = source.indexOf("\n", lineStart);
+		}
+	}
+	res.push(lineStart);
+	return res;
+}
