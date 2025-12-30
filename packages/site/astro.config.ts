a7b4c11f866aa0a18457e5c6a1f357a1e49e6427
Josh Goldberg ✨ <git@joshuakgoldberg.com>
2025-12-30 17:14:08 -0500

    docs: add Team page (#1184)
    ## PR Checklist
    
    - [x] Addresses an existing open issue: fixes #341
    - [x] That issue was marked as [`status: accepting
    prs`](https://github.com/flint-fyi/flint/issues?q=is%3Aopen+is%3Aissue+label%3A%22status%3A+accepting+prs%22)
    - [x] Steps in
    [CONTRIBUTING.md](https://github.com/flint-fyi/flint/blob/main/.github/CONTRIBUTING.md)
    were taken
    
    ## Overview
    
    ❤️‍🔥
    
    ---------
    
    Co-authored-by: auvred <61150013+auvred@users.noreply.github.com>

--- packages/site/astro.config.ts
+++ packages/site/astro.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
 					authors: {
 						joshuakgoldberg: {
 							name: "Josh Goldberg",
-							picture: "/josh.webp",
+							picture: "/team/joshuakgoldberg.webp",
 							title: "Creator & Maintainer",
 							url: "https://joshuakgoldberg.com",
 						},
@@ -42,6 +42,7 @@ export default defineConfig({
 								{ label: "Configuration", link: "configuration" },
 								{ label: "Glossary", link: "glossary" },
 								{ label: "FAQs", link: "faqs" },
+								{ label: "Team", link: "team" },
 							],
 							label: "About",
 							link: "about",
