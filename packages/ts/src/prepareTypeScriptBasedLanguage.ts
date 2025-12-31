a3545b8421d7c2ee134eeea8e87f163090812f1d
auvred <aauvred@gmail.com>
2025-12-31 13:18:53 +0300

    patch fs instead of copying TypeScript source code

--- packages/ts/src/prepareTypeScriptBasedLanguage.ts
+++ packages/ts/src/prepareTypeScriptBasedLanguage.ts
@@ -2,11 +2,12 @@ import { createProjectService } from "@typescript-eslint/project-service";
 import { CachedFactory } from "cached-factory";
 import { debugForFile } from "debug-for-file";
 import path from "node:path";
+import fs from "node:fs";
+import assert from "node:assert/strict";
 import * as ts from "typescript";
 
 import { createTypeScriptFileFromProjectService } from "./createTypeScriptFileFromProjectService.js";
 import { LinterHost } from "@flint.fyi/core";
-import { matchFiles } from "./typescriptUtilities.js";
 
 const log = debugForFile(import.meta.filename);
 
@@ -24,6 +25,18 @@ function notImplemented(methodName: string): any {
 		`Flint bug: ts.System's method '${methodName}' is not implemented.`,
 	);
 }
+
+// https://github.com/nodejs/node/blob/7b7f693a98da060e19f2ec12fb99997d5d5524f9/deps/uv/include/uv.h#L1260-L1269
+const UV_DIRENT_TYPE = {
+	UV_DIRENT_UNKNOWN: 0,
+	UV_DIRENT_FILE: 1,
+	UV_DIRENT_DIR: 1,
+};
+
+const DirentCtor = fs.Dirent as {
+	// https://github.com/nodejs/node/blob/7b7f693a98da060e19f2ec12fb99997d5d5524f9/lib/internal/fs/utils.js#L160
+	new (name: string, type: number, parentPath: string): fs.Dirent;
+};
 
 export function prepareTypeScriptBasedLanguage(
 	host: LinterHost,
@@ -61,31 +74,55 @@ export function prepareTypeScriptBasedLanguage(
 					"file"
 				);
 			},
-			readDirectory(directoryPath, extensions, excludes, includes, depth) {
-				return matchFiles(
-					directoryPath,
-					extensions,
-					excludes,
-					includes,
-					ts.sys.useCaseSensitiveFileNames,
-					host.getCurrentDirectory(),
-					depth,
-					(directoryPath) => {
-						const files: string[] = [];
-						const directories: string[] = [];
-						for (const dirent of host.readDirectory(
-							path.resolve(host.getCurrentDirectory(), directoryPath),
-						)) {
-							if (dirent.type === "file") {
-								files.push(dirent.name);
-							} else if (dirent.type === "directory") {
-								directories.push(dirent.name);
-							}
-						}
-						return { files, directories };
-					},
-					ts.sys.realpath ?? ((path) => path),
-				);
+			readDirectory(directoryPath, extensions, exclude, include, depth) {
+				const originalCwd = process.cwd;
+				process.cwd = () => host.getCurrentDirectory();
+				const originalReadDirSync = fs.readdirSync;
+				// @ts-expect-error - TypeScript doesn't understand that the overloads do match up.
+				const patchedReaddirSync: typeof fs.readdirSync = (
+					readPath,
+					options,
+				) => {
+					assert.deepEqual(
+						options,
+						{ withFileTypes: true },
+						"Flint bug: ts.sys.readDirectory passed unexpected options to fs.readdirSync",
+					);
+					assert.ok(
+						typeof readPath === "string",
+						"Flint bug: ts.sys.readDirectory passed unexpected path to fs.readdirSync",
+					);
+					fs.readdirSync = originalReadDirSync;
+					try {
+						return host
+							.readDirectory(readPath)
+							.map(
+								(dirent) =>
+									new DirentCtor(
+										dirent.name,
+										dirent.type === "file"
+											? UV_DIRENT_TYPE.UV_DIRENT_FILE
+											: UV_DIRENT_TYPE.UV_DIRENT_DIR,
+										readPath,
+									),
+							);
+					} finally {
+						fs.readdirSync = patchedReaddirSync;
+					}
+				};
+				fs.readdirSync = patchedReaddirSync;
+				try {
+					return ts.sys.readDirectory(
+						directoryPath,
+						extensions,
+						exclude,
+						include,
+						depth,
+					);
+				} finally {
+					process.cwd = originalCwd;
+					fs.readdirSync = originalReadDirSync;
+				}
 			},
 			setImmediate,
 			setTimeout,
@@ -133,42 +170,10 @@ export function prepareTypeScriptBasedLanguage(
 		},
 	});
 
-	const servicePrograms = new CachedFactory((filePathAbsolute: string) => {
-		log("Opening client file:", filePathAbsolute);
-		service.openClientFile(filePathAbsolute);
-
-		log("Retrieving client services:", filePathAbsolute);
-		const scriptInfo = service.getScriptInfo(filePathAbsolute);
-		if (!scriptInfo) {
-			throw new Error(
-				`Could not find script info for file: ${filePathAbsolute}`,
-			);
-		}
-
-		const defaultProject = service.getDefaultProjectForFile(
-			scriptInfo.fileName,
-			true,
-		);
-		if (!defaultProject) {
-			throw new Error(
-				`Could not find default project for file: ${filePathAbsolute}`,
-			);
-		}
-
-		const program = defaultProject.getLanguageService(true).getProgram();
-		if (!program) {
-			throw new Error(
-				`Could not retrieve program for file: ${filePathAbsolute}`,
-			);
-		}
-
-		return program;
-	});
-
 	return {
 		createFile: (filePathAbsolute) => {
 			log("Opening client file:", filePathAbsolute);
-			service.openClientFile(filePathAbsolute); //, host.readFile(filePathAbsolute))
+			service.openClientFile(filePathAbsolute);
 
 			log("Retrieving client services:", filePathAbsolute);
 			const scriptInfo = service.getScriptInfo(filePathAbsolute);
@@ -195,11 +200,20 @@ export function prepareTypeScriptBasedLanguage(
 				);
 			}
 
-			return createTypeScriptFileFromProjectService(
-				filePathAbsolute,
+			const sourceFile = program.getSourceFile(filePathAbsolute);
+			if (!sourceFile) {
+				throw new Error(
+					`Could not retrieve source file for: ${filePathAbsolute}`,
+				);
+			}
+
+			return {
 				program,
-				service,
-			);
+				sourceFile,
+				[Symbol.dispose]() {
+					service.closeClientFile(filePathAbsolute);
+				},
+			};
 		},
 	};
 }
