ae5c5a16b19f93c7970532cef23b3aba2251ad75
Josh Goldberg <git@joshuakgoldberg.com>
2026-01-05 11:14:21 -0500

    Move back to no-deprecated override

--- eslint.config.ts
+++ eslint.config.ts
@@ -76,17 +76,6 @@ export default defineConfig(
 			},
 		},
 		rules: {
-			"@typescript-eslint/no-deprecated": [
-				"error",
-				{
-					allow: [
-						{
-							from: "lib",
-							name: "builtinRules",
-						},
-					],
-				},
-			],
 			"@typescript-eslint/no-import-type-side-effects": "error",
 			"@typescript-eslint/no-unnecessary-condition": [
 				"error",
--- packages/comparisons/src/compat.test.ts
+++ packages/comparisons/src/compat.test.ts
@@ -10,6 +10,8 @@ describe("data.json", () => {
 	describe("Comparison with ESLint", () => {
 		it("includes all builtin rules", () => {
 			const builtinESLintRuleNames = new Set<string>(
+				// builtinRules is marked as deprecated since it's in "use-at-your-own-risk", not actually deprecated
+				// eslint-disable-next-line @typescript-eslint/no-deprecated
 				[...builtinRules]
 					.flatMap(([ruleName, module]) =>
 						!module.meta?.deprecated ? [ruleName] : [],
