7a6646c3e97834078e2873ba1e2d7a05a78efaae
Josh Goldberg <git@joshuakgoldberg.com>
2026-01-07 17:27:48 -0500

    more extension fixes

--- packages/md/src/rules/referenceLikeUrls.test.ts
+++ packages/md/src/rules/referenceLikeUrls.test.ts
@@ -1,5 +1,5 @@
-import rule from "./referenceLikeUrls.js";
-import { ruleTester } from "./ruleTester.js";
+import rule from "./referenceLikeUrls.ts";
+import { ruleTester } from "./ruleTester.ts";
 
 ruleTester.describe(rule, {
 	invalid: [
