3acbd9ea59e8a6faaecb3b3f00852d85d8b094c1
Josh Goldberg <git@joshuakgoldberg.com>
2026-01-07 13:51:42 -0500

    feat(ts): implement caughtVariableNames rule
    Closes #1307

--- packages/ts/src/plugin.ts
+++ packages/ts/src/plugin.ts
@@ -4,6 +4,7 @@ import anyReturns from "./rules/anyReturns.ts";
 import asyncPromiseExecutors from "./rules/asyncPromiseExecutors.ts";
 import caseDeclarations from "./rules/caseDeclarations.ts";
 import caseDuplicates from "./rules/caseDuplicates.ts";
+import caughtVariableNames from "./rules/caughtVariableNames.ts";
 import chainedAssignments from "./rules/chainedAssignments.ts";
 import classAssignments from "./rules/classAssignments.ts";
 import consecutiveNonNullAssertions from "./rules/consecutiveNonNullAssertions.ts";
@@ -68,6 +69,7 @@ export const ts = createPlugin({
 		asyncPromiseExecutors,
 		caseDeclarations,
 		caseDuplicates,
+		caughtVariableNames,
 		chainedAssignments,
 		classAssignments,
 		consecutiveNonNullAssertions,
