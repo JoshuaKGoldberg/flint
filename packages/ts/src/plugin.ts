b6e1d1bc70db976759d1206c9fa11da81dfef2b1
Josh Goldberg <git@joshuakgoldberg.com>
2026-01-09 17:58:32 -0500

    Missing plugin.ts listing

--- packages/ts/src/plugin.ts
+++ packages/ts/src/plugin.ts
@@ -24,6 +24,7 @@ import chainedAssignments from "./rules/chainedAssignments.ts";
 import classAssignments from "./rules/classAssignments.ts";
 import classFieldDeclarations from "./rules/classFieldDeclarations.ts";
 import classLiteralProperties from "./rules/classLiteralProperties.ts";
+import classMemberDuplicates from "./rules/classMemberDuplicates.ts";
 import combinedPushes from "./rules/combinedPushes.ts";
 import consecutiveNonNullAssertions from "./rules/consecutiveNonNullAssertions.ts";
 import constantAssignments from "./rules/constantAssignments.ts";
@@ -111,6 +112,7 @@ export const ts = createPlugin({
 		classAssignments,
 		classFieldDeclarations,
 		classLiteralProperties,
+		classMemberDuplicates,
 		combinedPushes,
 		consecutiveNonNullAssertions,
 		constantAssignments,
